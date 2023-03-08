1c3417a8fae06e42081ed09deaac9e236af462dc
Bogdan <bsshul@gmail.com>
2023-03-09 01:44:54 +0200

    Fixed style

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -64,15 +64,16 @@ export default function Meal({ daylyMeals, lastDishes }) {
           </tbody>
         </table>
       </div>
-      <div className="flex fixed w-full -bottom-[6px] md:bottom-2">
-        <div className="flex flex-nowrap overflow-x-auto pl-3">
-          <div className="mt-1 mr-2">
+
+      <div className="flex fixed w-full bottom-1 overflow-hidden">
+        <div className="flex flex-nowrap overflow-x-auto -mb-2 sm:mb-0 pl-3">
+          <div className="mt-[2px] mr-2">
             <HeartIcon style={{ fill: "#EF4444" }} className="h-8 w-8" />
           </div>
           {favoriteProducts.map((product) => (
             <div
               key={product.id}
-              className="whitespace-nowrap text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 font-medium rounded-full text-xs px-5 py-2 mr-2 mb-2 cursor-pointer"
+              className="whitespace-nowrap text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 font-medium rounded-full text-sm px-3 py-2 mr-1 mb-2 cursor-pointer"
             >
               {product.title}
             </div>
