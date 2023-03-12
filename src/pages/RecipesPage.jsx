c7e225dd371e47f40ee22581d3647f0a7e5a297c
Bogdan <bsshul@gmail.com>
2023-03-12 10:53:58 +0300

    Added info

--- src/pages/RecipesPage.jsx
+++ src/pages/RecipesPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function RecipesPage() {
-  return <div>RecipesPage</div>;
+  return <div className="flex justify-center pt-32">Сторiнка в розробцi</div>;
 }
